6056acfb5cf0ab2be2093d1d49da21fc6f4e3a3c
Карабас <da_om@mail.ru>
2023-07-11 16:42:32 +0300

    Бизли, Джонс. Книга рецептов. 8.8 Расширение свойства в подклассе. (Наследование)

---
+++ test02.py
@@ -0,0 +1,46 @@
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    # функция - геттер
+    @property
+    def name(self):
+        return self.name
+
+    # функция - сеттер
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Expected a string')
+        self._name = value
+
+    # Функция - делитер
+    @name.deleter
+    def name(self):
+        raise AttributeError("Can't delete attribute")
+
+
+# Вот пример класса, который наследует от Person и расширяет свойство # name новой функциональностью:
+
+
+class SubPerson(Person):
+    @property
+    def name(self):
+        print('Getting name')
+        return super().name
+
+    @name.setter
+    def name(self, value):
+        print('Setting name to', value)
+        super(SubPerson, SubPerson).name.__set__(self, value)
+
+    @name.deleter
+    def name(self):
+        print('Deleting name')
+        super(SubPerson, SubPerson).name.__delete__(self)
+
+
+s = SubPerson('Guido')
+
+print(s)
